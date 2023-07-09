32c00df77bd9f844449a731476c0c842913f3af9
Aleksandra <91892932+aleksandragoryczka@users.noreply.github.com>
2023-07-09 12:12:21 +0200

    init commit

---
+++ backend/mindsync/src/main/java/com/project/mindsync/MindsyncApplication.java
@@ -0,0 +1,13 @@
+package com.project.mindsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindsyncApplication.class, args);
+	}
+
+}
---
+++ backend/mindsync/src/test/java/com/project/mindsync/MindsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mindsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
