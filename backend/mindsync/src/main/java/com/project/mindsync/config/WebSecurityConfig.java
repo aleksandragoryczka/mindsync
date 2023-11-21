db92e771f4fd39c493f51e38d8df9e3ee836db7a
Aleksandra <91892932+aleksandragoryczka@users.noreply.github.com>
2023-11-21 20:48:39 +0100

    Update WebSecurityConfig.java

--- backend/mindsync/src/main/java/com/project/mindsync/config/WebSecurityConfig.java
+++ backend/mindsync/src/main/java/com/project/mindsync/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.project.mindsync.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -59,15 +60,16 @@ public class WebSecurityConfig {
 		http.csrf(csrf -> csrf.disable()).cors(cors -> cors.disable())
 				.exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.authorizeHttpRequests(auth -> auth.requestMatchers(new AntPathRequestMatcher("/swagger-ui"))
-						.permitAll().requestMatchers(new AntPathRequestMatcher("/v3/api/docs/**")).permitAll()
-						.requestMatchers(new AntPathRequestMatcher("/swagger-ui.html")).permitAll()
-						.requestMatchers(new AntPathRequestMatcher("/ws/**")).permitAll()
-						.requestMatchers(new AntPathRequestMatcher("/api/auth/**")).permitAll()
-						.requestMatchers(new AntPathRequestMatcher("/api/quiz/**")).permitAll()
-						.requestMatchers(new AntPathRequestMatcher("/api/show/**")).authenticated()
-						.requestMatchers(new AntPathRequestMatcher("/api/slide/**")).authenticated()
-						.requestMatchers(new AntPathRequestMatcher("/api/user/**")).permitAll());
+				.authorizeHttpRequests(
+						auth -> auth.requestMatchers(HttpMethod.OPTIONS).permitAll().requestMatchers("/api/slide/**")
+								.authenticated().requestMatchers(new AntPathRequestMatcher("/api/show/**"))
+								.authenticated().requestMatchers(new AntPathRequestMatcher("/swagger-ui")).permitAll()
+								.requestMatchers(new AntPathRequestMatcher("/v3/api/docs/**")).permitAll()
+								.requestMatchers(new AntPathRequestMatcher("/swagger-ui.html")).permitAll()
+								.requestMatchers(new AntPathRequestMatcher("/ws/**")).permitAll()
+								.requestMatchers(new AntPathRequestMatcher("/api/auth/**")).permitAll()
+								.requestMatchers(new AntPathRequestMatcher("/api/quiz/**")).permitAll()
+								.requestMatchers(new AntPathRequestMatcher("/api/user/**")).permitAll());
 
 		http.authenticationProvider(authenticationProvider());
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
